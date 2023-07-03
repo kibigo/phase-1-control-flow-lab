1d3b5e808fa2ca6a0173472df43a30204e2ca5ec
Kibigo <jacksonkibigo99@gmail.com>
2023-07-03 21:56:58 +0300

    first commit

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 function scuberGreetingForFeet(distance){
-  if (distance <=400){
+  if (distance <= 400 ){
+
     return "This one is on me!"
-  }else if (distance >2000 && distance < 2500){
+  } else if (distance > 2000 && distance < 2500){
     return "I will gladly take your thirty bucks."
-  }else {
+  } else {
     return "No can do."
   }
-  
 }
 
 function ternaryCheckCity(city){
-  if (city === 'NYC'){
+  if (city == "NYC"){
     return "Ok, sounds good."
-  }else{
+  } else if (city !== "NYC"){
     return "No go."
   }
 }
 
 function switchOnCharmFromTip(tip){
-  if (tip === 'generous'){
+  if (tip == "generous"){
     return "Thank you so much."
-  }else if (tip === 'not as generous'){
+  }else if (tip == "not as generous"){
     return "Thank you."
-  }else if (tip !== 'generous'){
+  }else{
     return "Bye."
   }
 }
